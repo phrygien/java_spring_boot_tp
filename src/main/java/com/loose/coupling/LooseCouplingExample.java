d8e45620eb4f0da8960d9e345c3ebaa99b72f85e
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-08-20 15:38:11 +0300

    tp: loosly coupling

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -5,5 +5,9 @@ public class LooseCouplingExample {
         UserDataProvider databaseProvider = new UserDatabaseProvider();
         UserManager userManagerWithDB = new UserManager(databaseProvider);
         System.out.println(userManagerWithDB.getUserInfo());
+
+        UserDataProvider webServiceProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+        System.out.println(userManagerWithWS.getUserInfo());
     }
 }
